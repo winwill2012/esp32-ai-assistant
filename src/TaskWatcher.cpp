f7048c1e867f0e530511bc255d9f80a84ef9378c
戚辅光 <9330@9ji.com>
2025-02-18 20:37:26 +0800

    语音对话调通

---
+++ src/TaskWatcher.cpp
@@ -0,0 +1,70 @@
+#include "TaskWatcher.h"
+
+#include <utility>
+
+void checkChatTask(void *ptr) {
+    auto *watcher = (TaskWatcher *) ptr;
+    String *message;
+    while (true) {
+        if (xQueueReceive(watcher->getChatQueue(), &message, portMAX_DELAY) == pdTRUE) {
+            Serial.printf("收到chat任务: %s\n", (*message).c_str());
+            AgentResponse *response = watcher->getLLMAgent().chat(*message);
+            free(message);
+            Serial.print("LLMAgent返回结果: ");
+            Serial.println(response->response);
+            if (response->response != "") {
+                auto *task = new String(response->response);
+                watcher->publishTTSTask(task);
+            }
+            vTaskDelay(1);
+        }
+    }
+}
+
+void checkTTSTask(void *ptr) {
+    auto *watcher = (TaskWatcher *) ptr;
+    String *message;
+    while (true) {
+        if (xQueueReceive(watcher->getTTSQueue(), &message, portMAX_DELAY) == pdTRUE) {
+            Serial.printf("收到tts任务: %s\n", (*message).c_str());
+            watcher->getTTS().synth(*message);
+            free(message);
+            vTaskDelay(1);
+        }
+    }
+}
+
+TaskWatcher::TaskWatcher(const LLMAgent &llmAgent, DoubaoTTS tts) : _llmAgent(llmAgent), _tts(std::move(tts)) {
+    _chatQueue = xQueueCreate(3, sizeof(String *));
+    _ttsQueue = xQueueCreate(3, sizeof(String *));
+    xTaskCreate(checkChatTask, "checkChatTask", 4096, this, 1, nullptr);
+    xTaskCreate(checkTTSTask, "checkTTSTask", 4096, this, 1, nullptr);
+}
+
+TaskWatcher::~TaskWatcher() = default;
+
+void TaskWatcher::publishChatTask(String *text) {
+    Serial.printf("往chat队列发送消息: %s\n", (*text).c_str());
+    xQueueSend(_chatQueue, &text, portMAX_DELAY);
+}
+
+void TaskWatcher::publishTTSTask(String *message) {
+    Serial.printf("往tts队列发送消息: %s\n", (*message).c_str());
+    xQueueSend(_ttsQueue, &message, portMAX_DELAY);
+}
+
+QueueHandle_t TaskWatcher::getChatQueue() {
+    return _chatQueue;
+}
+
+QueueHandle_t TaskWatcher::getTTSQueue() {
+    return _ttsQueue;
+}
+
+LLMAgent TaskWatcher::getLLMAgent() {
+    return _llmAgent;
+}
+
+DoubaoTTS TaskWatcher::getTTS() {
+    return _tts;
+}
---
+++ src/TaskWatcher.h
@@ -0,0 +1,35 @@
+#ifndef IOT_AI_CONTROLLER_TASKWATCHER_H
+#define IOT_AI_CONTROLLER_TASKWATCHER_H
+
+#include <Arduino.h>
+#include "LLMAgent.h"
+#include "DoubaoTTS.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+
+class TaskWatcher {
+public :
+    TaskWatcher(const LLMAgent &llmAgent, DoubaoTTS tts);
+
+    ~TaskWatcher();
+
+    void publishChatTask(String *text);
+
+    void publishTTSTask(String *message);
+
+    QueueHandle_t getChatQueue();
+
+    QueueHandle_t getTTSQueue();
+
+    LLMAgent getLLMAgent();
+
+    DoubaoTTS getTTS();
+
+private :
+    LLMAgent _llmAgent;
+    DoubaoTTS _tts;
+    QueueHandle_t _chatQueue;
+    QueueHandle_t _ttsQueue;
+};
+
+#endif
